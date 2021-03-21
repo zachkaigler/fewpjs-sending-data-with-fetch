1134de30f8f47b664e8ad457315ed2d38b084d73
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-21 19:38:40 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData (userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`
+        })
+    }).then(function(response) {
+        return response.json();
+    }).then(function(object) {
+        let body = document.querySelector("body")
+        body.append(object.id)
+    }).catch(function(error) {
+        let body = document.querySelector("body")
+        body.append(error.message)
+    })
+}
